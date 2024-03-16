15c1ca91273d55d724f9a04ee4519061d835b758
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-16 09:51:53 -0300

    feat: add member table

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,69 @@ const App = () => {
     "edition-drop"
   )
 
+  const tokenAddress = "0x7324De0Bb0aAD8b1A86bA6fB5a334BB08D26c603"
+  const { contract: token } = useContract(tokenAddress, "token")
+
   const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
 
   const hasClaimedNFT = useMemo(() => {
     return nftBalance && nftBalance.gt(0)
   }, [nftBalance])
+
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState([])
+  const [memberAddresses, setMemberAddresses] = useState([])
+
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + "..." + str.substring(str.length - 4)
+  }
+
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return
+    }
+
+    const getAllAddresses = async () => {
+      try {
+        const memberAddresses =
+          await editionDrop.history.getAllClaimerAddresses(0)
+        setMemberAddresses(memberAddresses)
+        console.log("🚀 Endereços de membros", memberAddresses)
+      } catch (error) {
+        console.error("falha ao pegar lista de membros", error)
+      }
+    }
+    getAllAddresses()
+  }, [hasClaimedNFT, editionDrop])
+
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return
+    }
+
+    const getAllBalances = async () => {
+      try {
+        const amounts = await token.history.getAllHolderBalances()
+        setMemberTokenAmounts(amounts)
+        console.log("👜 Quantidades", amounts)
+      } catch (error) {
+        console.error("falha ao buscar o saldo dos membros", error)
+      }
+    }
+    getAllBalances()
+  }, [hasClaimedNFT, token])
+
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      const member = memberTokenAmounts?.find(
+        ({ holder }) => holder === address
+      )
+
+      return {
+        address,
+        tokenAmount: member?.balance.displayValue || "0",
+      }
+    })
+  }, [memberAddresses, memberTokenAmounts])
 
   return (
     <div className="w-100 h-[100vh] flex flex-col justify-center items-center bg-green-100">
@@ -31,10 +89,32 @@ const App = () => {
             <h1 className="text-4xl font-bold mb-2">
               Página de membros PrensaDAO
             </h1>
-            <h3 className="text-xl mb-4">
+            <h3 className="text-md mb-4">
               parabéns por fazer parte desse clube
             </h3>
             <ConnectWallet theme={"light"} />
+
+            <h2 className="text-2xl font-medium mt-12 mb-4">Membros</h2>
+            <table className="w-[400px] text-sm text-gray-500 rounded-lg border overflow-hidden">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3">Endereço</th>
+                  <th className="px-6 py-3">Quantidade de tokens</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => (
+                  <tr className="bg-white border-b" key={member.address}>
+                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                      {shortenAddress(member.address)}
+                    </td>
+                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                      {member.tokenAmount}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </>
         ) : (
           <>
